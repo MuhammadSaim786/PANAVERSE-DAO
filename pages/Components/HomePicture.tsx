8f7ee103f679e3b621c051f6a452f4b283b12ddf
MuhammadSaim786 <saim66381@gmail.com>
2023-02-06 20:36:38 -0800

    first commit

---
+++ pages/Components/HomePicture.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from "../../styles/HomeTitle.module.css"
+import { Slide } from "react-awesome-reveal";
+import Image from 'next/image';
+
+import {Box, Flex, Heading} from '@chakra-ui/react'
+const HomePicture = () => {
+  return (
+  
+   <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet}  overflow="hidden">
+         <Flex justifyContent={"space-between"}>
+                <Box className={styles.mainHeadingSet}  >
+        <Slide> <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
+<> Certified Web3 and Metaverse Developer </>
+        </Heading> </Slide>
+        </Box>
+  
+  <Box className={styles.mainHeadingSet}  >
+        <Slide direction='right'> <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
+       < > In Karachi, Lahore, Islamabad, and Peshawar </>       </Heading>
+       </Slide>
+        </Box>
+        </Flex> 
+        
+     </Box>
+  
+  )
+}
+
+export default HomePicture
+
+    
+   
+
